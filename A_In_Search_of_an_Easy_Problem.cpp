f8bca056f97542e1c1cacf432a54fc191b501e37
M.G senith deelaka abeysinghe <senithdeelaka10@gmail.com>
2024-01-17 16:19:43 +0530

    2024/1/17

---
+++ A_In_Search_of_an_Easy_Problem.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+int main(){
+    int n,tmp;
+    std::cin>>n;
+    while(std::cin>>tmp){
+        if(tmp==1)
+            {
+                std::cout<< "HARD"<<"\n";
+                return 0;
+            }
+    }
+    std::cout<<"EASY"<<"\n";
+    return 0;
+}
